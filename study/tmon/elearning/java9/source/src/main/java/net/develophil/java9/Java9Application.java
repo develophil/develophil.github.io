af4741fb423b27c1ada07b70648adf010cabbdd9
hkpking <hkpking@tmon.co.kr>
2018-10-12 18:46:00 +0900

    java source module 추가

---
+++ study/tmon/elearning/java9/source/src/main/java/net/develophil/java9/Java9Application.java
@@ -0,0 +1,12 @@
+package net.develophil.java9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java9Application.class, args);
+	}
+}
