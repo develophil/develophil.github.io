3a6923118bd3a5c8482d5a3a8fef11f3132359e7
Unknown <hkpking@tmon.co.kr>
2019-07-10 18:19:38 +0900

    03. jumping on the clouds problem

---
+++ JavaCodingTester/src/hackerrank/warm_up_challenges/no03_jumping_on_the_clouds/Solution.java
@@ -0,0 +1,42 @@
+package hackerrank.warm_up_challenges.no03_jumping_on_the_clouds;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+
+        return 0;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
